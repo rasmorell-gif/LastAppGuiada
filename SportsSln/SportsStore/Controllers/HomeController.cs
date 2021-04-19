829dbb1a6facda4def8624a8450268ebeebd9031
rasmorell-gif <61146061+rasmorell-gif@users.noreply.github.com>
2021-04-19 15:37:58 -0400

    Ultima aplicacion guiada

---
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers {
+    public class HomeController: Controller {
+
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo) {
+            repository = repo;
+        }
+        public IActionResult Index() => View(repository.Products);
+    } 
+}
---
+++ SportsSln/SportsStore/Models/ IStoreRepository.cs
@@ -0,0 +1,6 @@
+using System.Linq;
+namespace SportsStore.Models {
+    public interface IStoreRepository {
+        IQueryable<Product> Products { get; }
+    }
+}
